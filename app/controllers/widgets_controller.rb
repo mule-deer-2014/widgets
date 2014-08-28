016fb0836edb79b43a0f10bdf91c3f553c5a2032
Steven G. Harms <steven@devbootcamp.com>
2014-08-27 17:12:45 -0700

    Add Widgets#create

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,4 +2,19 @@ class WidgetsController < ApplicationController
   def new
     @widget = Widget.new
   end
+
+  def create
+    @widget = Widget.create(widget_params)
+    if @widget.save
+      redirect_to widget_path(@widget)
+    else
+      redirect_to new_widget_path(@widget)
+    end
+  end
+
+  private
+
+  def widget_params
+    params.require(:widget).permit(:body)
+  end
 end
